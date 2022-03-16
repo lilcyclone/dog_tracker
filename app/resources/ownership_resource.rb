4e7ca3dc4d9e36225d1d730a5ac7ec25c98dfdde
anonymous anonymous <github@xindygao.com>
2022-03-16 01:24:36 +0000

    Direct association between UserResource and OwnershipResource

--- app/resources/ownership_resource.rb
+++ app/resources/ownership_resource.rb
@@ -7,6 +7,8 @@ class OwnershipResource < ApplicationResource
 
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
 end
--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -8,6 +8,8 @@ class UserResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :ownerships
+
   # Indirect associations
 
 end
